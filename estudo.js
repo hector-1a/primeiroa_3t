62e28b8226e99d1f14ecab54ffb300b76df0aad9
hector-1a <111132223+hector-1a@users.noreply.github.com>
2022-10-05 08:19:15 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,1 +1,33 @@
+let tabuada = 0.1; 
+let nome = "hector";
+
+
+function escrever(){
+    document.write("tabuada do "+nome)
+    document.write(tabuada+" x 1 = "+ (tabuada*1)+"<br>");
+    document.write(tabuada+" x 2 = "+ (tabuada*2)+"<br>");
+    document.write(tabuada+" x 3 = "+ (tabuada*3)+"<br>");
+    document.write(tabuada+" x 4 = "+ (tabuada*4)+"<br>");
+    document.write(tabuada+" x 5 = "+ (tabuada*5)+"<br>");
+    document.write(tabuada+" x 6 = "+ (tabuada*6)+"<br>");
+    document.write(tabuada+" x 7 = "+ (tabuada*7)+"<br>");
+    document.write(tabuada+" x 8 = "+ (tabuada*8)+"<br>");
+    document.write(tabuada+" x 9 = "+ (tabuada*9)+"<br>");
+    document.write(tabuada+" x 10 = "+ (tabuada*10)+"<br>");
+    
+}
+
+function minhaTabuada(){
+    for(let i = 1; i<= 10; 1++){
+  document.write("tabuada do "+ i + "<br>");
+        for(let j = 1; j <=10; j++){
+document.write(1 + "x" +j+ "=" +(j*i)+"<br>");
+
+             }
+      }       
+   }    
+   function quadrado()}
+   for(let i = 1 ; i <= 20; i++){
+       document.write("o quadrado de "+ i +"é"+(i*i)+"<br>");
+   }
 
